37ef1101ffe434377c7007ebbf997b837bf2bb90
Ian Preston <17241371+cornucrapia@users.noreply.github.com>
2018-07-27 15:42:03 -0600

    in friendly installer format

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'stats_can',
+  author = 'Ian Preston',
+  description = 'Access StatsCan data with python',
+  license = 'GPL',
+  packages = find_packages()
+  )
---
+++ stats_can/__init__.py
@@ -0,0 +1,14 @@
+'''Read StatsCan Data into python, mostly pandas dataframes'''
+__version__ = '0.1'
+
+from .stats_can import get_tables_for_vectors
+from .stats_can import table_subsets_from_vectors
+from .stats_can import get_cube_metadata
+from .stats_can import get_series_info_from_vector
+from .stats_can import get_changed_series_list
+from .stats_can import get_changed_cube_list
+from .stats_can import vectors_to_df
+from .stats_can import download_tables
+from .stats_can import update_tables
+from .stats_can import table_to_dataframe
+from .stats_can import get_classic_vector_format_df
