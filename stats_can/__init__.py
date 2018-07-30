71c97df1e9699ce418849c67bcbf20ea259053e8
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2018-07-30 15:20:14 -0600

    changed import order
    matches the order they're listed in the StatsCan api and the stats_can module

--- stats_can/__init__.py
+++ stats_can/__init__.py
@@ -1,12 +1,13 @@
 '''Read StatsCan Data into python, mostly pandas dataframes'''
 __version__ = '0.1'
 
-from .stats_can import get_tables_for_vectors
-from .stats_can import table_subsets_from_vectors
-from .stats_can import get_cube_metadata
-from .stats_can import get_series_info_from_vector
 from .stats_can import get_changed_series_list
 from .stats_can import get_changed_cube_list
+from .stats_can import get_cube_metadata
+from .stats_can import get_series_info_from_vector
+from .stats_can import get_bulk_vector_data_by_range
+from .stats_can import get_tables_for_vectors
+from .stats_can import table_subsets_from_vectors
 from .stats_can import vectors_to_df
 from .stats_can import download_tables
 from .stats_can import update_tables
