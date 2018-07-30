6728a51f006ad698b487af6d5b7158fd9217b275
Ian Preston <17241371+cornucrapia@users.noreply.github.com>
2018-07-30 12:59:38 -0600

    Function order matches StatsCan docs, improved docstring

--- stats_can/stats_can.py
+++ stats_can/stats_can.py
@@ -1,15 +1,42 @@
 # -*- coding: utf-8 -*-
-"""
-Provides a python implementation of the Statistics Canada Web Data Service
+""" Provides a python implementation of the Statistics Canada Web Data Service
+
 https://www.statcan.gc.ca/eng/developers/wds/user-guide
+
 Note: StatsCan uses cube/table interchangeably. I'm going to keep cube in my
 function names where it maps to their api but otherwise I will use table.
 Hence functions with cube in the function name will take tables as an argument
 I'm not sure which is less confusing, it's annoying they weren't just
 consistent.
-ToDo: Oh man, so much. Some of the api is implemented, but lots is missing
-Still have to figure out some conventions, and how much to put in each api
-function in terms of cleanup
+
+# noqa on some function definitions as they don't pass pylint, but I want to
+keep the long function names that match the actual WDS
+
+
+Attributes
+----------
+SC_URL : str
+    URL for the Statistics Canada REST api
+
+TODO
+----
+Oh man, so much.
+Missing api implementations:
+    GetSeriesInfoFromCubePidCoord
+    GetChangedSeriesDataFromCubePidCoord
+    GetChangedSeriesDataFromVector
+    GetDataFromCubePidCoordAndLatestNPeriods
+    GetDataFromVectorsAndLatestNPeriods (Next on my list)
+    GetFullTableDownloadSDMX
+    GetCodeSets
+I'd like to look into saving downloaded tables as hdf rather than csv/json
+If it works and is reasonably space efficient it would save processing time
+
+Functions that are still just returning JSON should return a dict or
+something nicer
+
+The whole thing only works in English right now. Add French support
+
 @author: Ian Preston
 """
 import re
@@ -24,15 +51,166 @@ import requests
 SC_URL = 'https://www150.statcan.gc.ca/t1/wds/rest/'
 
 
-def parse_tables(tables):
+def get_changed_series_list():
+    """https://www.statcan.gc.ca/eng/developers/wds/user-guide#a10-1"""
+    url = SC_URL + 'getChangedSeriesList'
+    result = requests.get(url)
+    result.raise_for_status()
+    return result.json()
+
+
+def get_changed_cube_list(date=dt.date.today()):
+    """https://www.statcan.gc.ca/eng/developers/wds/user-guide#a10-2"""
+    url = SC_URL + 'getChangedCubeList' + '/' + str(date)
+    result = requests.get(url)
+    result.raise_for_status()
+    return result
+
+
+def get_cube_metadata(tables):
+    """https://www.statcan.gc.ca/eng/developers/wds/user-guide#a11-1
+
+    Take a list of tables and return a list of dictionaries with their
+    metadata
     """
+    tables = parse_tables(tables)
+    tables = [{'productId': t} for t in tables]
+    url = SC_URL + 'getCubeMetadata'
+    result = requests.post(url, json=tables)
+    result.raise_for_status()
+    return result.json()
+
+
+def get_series_info_from_cube_pid_coord():  # noqa
+    """ Not implemented yet
+
+    https://www.statcan.gc.ca/eng/developers/wds/user-guide#a11-2"""
+    pass
+
+
+def get_series_info_from_vector(vectors):
+    """https://www.statcan.gc.ca/eng/developers/wds/user-guide#a11-3
+
+    Maxes out at 300 values so have to chunk it out
+    https://bit.ly/2sn5RS9
+    """
+    url = SC_URL + 'getSeriesInfoFromVector'
+    vectors = parse_vectors(vectors)
+    max_chunk = 300
+    chunks = [
+        vectors[i:i + max_chunk] for i in range(0, len(vectors), max_chunk)
+        ]
+    final_list = []
+    for chunk in chunks:
+        vectors = [{'vectorId': v} for v in chunk]
+        result = requests.post(url, json=vectors)
+        result.raise_for_status()
+        final_list += result.json()
+    return final_list
+
+
+def get_changed_series_data_from_cube_pid_coord():  # noqa
+    """ Not implemented yet
+
+    https://www.statcan.gc.ca/eng/developers/wds/user-guide#a12-1"""
+    pass
+
+
+def get_changed_series_data_from_vector():  # noqa
+    """ Not implemented yet
+
+    https://www.statcan.gc.ca/eng/developers/wds/user-guide#a12-2"""
+    pass
+
+
+def get_data_from_cube_pid_coord_and_latest_n_periods():  # noqa
+    """ Not implemented yet
+
+    https://www.statcan.gc.ca/eng/developers/wds/user-guide#a12-3
+    """
+    pass
+
+
+def get_data_from_vectors_and_latest_n_periods():  # noqa
+    """ Not implemented, next on my to do list
+
+    https://www.statcan.gc.ca/eng/developers/wds/user-guide#a12-4
+    """
+    pass
+
+
+def get_bulk_vector_data_by_range(
+        vector_ids, start_release_date, end_release_date
+):
+    """https://www.statcan.gc.ca/eng/developers/wds/user-guide#a12-5
+
+    ToDo: Add chunking to handle over 300 vectors
+    """
+    url = SC_URL + 'getBulkVectorDataByRange'
+    start_release_date = str(start_release_date) + "T13:00"
+    end_release_date = str(end_release_date) + "T13:00"
+    vector_ids = parse_vectors(vector_ids)
+    result = requests.post(
+        url,
+        json={
+            "vectorIds": vector_ids,
+            "startDataPointReleaseDate": start_release_date,
+            "endDataPointReleaseDate": end_release_date
+            }
+        )
+    return result.json()
+
+
+def get_full_table_download(table):
+    """https://www.statcan.gc.ca/eng/developers/wds/user-guide#a12-6
+
+    Take a table name and return a url to a zipped CSV of that table
+    """
+    table = parse_tables(table)
+    url = SC_URL + 'getFullTableDownloadCSV/' + table + '/en'
+    result = requests.get(url)
+    result = result.json()
+    if result['status'] != 'SUCCESS':
+        warnings.warn(str(result['object']))
+    return result['object']
+
+
+def get_full_table_download_sdmx():
+    """ Not implemented yet
+
+    https://www.statcan.gc.ca/eng/developers/wds/user-guide#a12-7
+    """
+    pass
+
+
+def get_code_sets():
+    """ Not implemented yet
+
+    https://www.statcan.gc.ca/eng/developers/wds/user-guide#a13-1
+    """
+    pass
+
+
+def parse_tables(tables):
+    """ Basic cleanup of table or tables to numeric
+
+
     Strip out hyphens or other non-numeric characters from a list of tables
     or a single table
     Table names in StatsCan often have a trailing -01 which isn't necessary
     So also take just the first 8 characters.
     This function by no means guarantees you have a clean list of valid tables,
     but it's a good start.
-    Returns a list of cleaned up table names or a string with cleaned name
+
+    Parameters
+    ----------
+    tables : list of str or str
+        A string or list of strings of table names to be parsed
+
+    Returns
+    -------
+    list of str
+        tables with unnecessary characters removed
     """
     def parse_table(table):
         """Clean up one table string"""
@@ -44,10 +222,20 @@ def parse_tables(tables):
 
 
 def parse_vectors(vectors):
-    """
-    Strip out V from V#s. If input is a string return a list with one entity
-    Similar to parse tables, this by no means guarantees a valid entry, just
-    helps with some standard input formats
+    """ Basic cleanup of vector or vectors
+
+    Strip out V from V#s. Similar to parse tables, this by no means guarantees
+    a valid entry, just helps with some standard input formats
+
+    Parameters
+    ----------
+    vectors : list of str or str
+        A string or list of strings of vector names to be parsed
+
+    Returns
+    -------
+    list of str
+        vectors with unnecessary characters removed
     """
     def parse_vector(vector):
         """Strip string to numeric elements only"""
@@ -89,98 +277,6 @@ def table_subsets_from_vectors(vectors):
     for vec in vecs:
         tables_dict[start_tables_dict[vec]].append(vec)
     return tables_dict
-
-
-def get_cube_metadata(tables):
-    """
-    https://www.statcan.gc.ca/eng/developers/wds/user-guide#a11-1
-    Take a list of tables and return a list of dictionaries with their
-    metadata
-    """
-    tables = parse_tables(tables)
-    tables = [{'productId': t} for t in tables]
-    url = SC_URL + 'getCubeMetadata'
-    result = requests.post(url, json=tables)
-    result.raise_for_status()
-    return result.json()
-
-
-def get_series_info_from_vector(vectors):
-    """
-    https://www.statcan.gc.ca/eng/developers/wds/user-guide#a11-3
-    Maxes out at 300 values so have to chunk it out
-    https://bit.ly/2sn5RS9
-    """
-    url = SC_URL + 'getSeriesInfoFromVector'
-    vectors = parse_vectors(vectors)
-    max_chunk = 300
-    chunks = [
-        vectors[i:i + max_chunk] for i in range(0, len(vectors), max_chunk)
-        ]
-    final_list = []
-    for chunk in chunks:
-        vectors = [{'vectorId': v} for v in chunk]
-        result = requests.post(url, json=vectors)
-        result.raise_for_status()
-        final_list += result.json()
-    return final_list
-
-
-def get_full_table_download(table):
-    """
-    https://www.statcan.gc.ca/eng/developers/wds/user-guide#a12-6
-    Take a table name and return a url to a zipped CSV of that table
-    """
-    table = parse_tables(table)
-    url = SC_URL + 'getFullTableDownloadCSV/' + table + '/en'
-    result = requests.get(url)
-    result = result.json()
-    if result['status'] != 'SUCCESS':
-        warnings.warn(str(result['object']))
-    return result['object']
-
-
-def get_changed_series_list():
-    """
-    https://www.statcan.gc.ca/eng/developers/wds/user-guide#a10-1
-    """
-    url = SC_URL + 'getChangedSeriesList'
-    result = requests.get(url)
-    result.raise_for_status()
-    return result.json()
-
-
-def get_changed_cube_list(date=dt.date.today()):
-    """
-    https://www.statcan.gc.ca/eng/developers/wds/user-guide#a10-2
-    """
-    url = SC_URL + 'getChangedCubeList' + '/' + str(date)
-    result = requests.get(url)
-    result.raise_for_status()
-    return result
-
-
-def get_bulk_vector_data_by_range(
-        vector_ids, start_release_date, end_release_date
-):
-    """
-    https://www.statcan.gc.ca/eng/developers/wds/user-guide#a12-5
-
-    ToDo: Add chunking to handle over 300 vectors
-    """
-    url = SC_URL + 'getBulkVectorDataByRange'
-    start_release_date = str(start_release_date) + "T13:00"
-    end_release_date = str(end_release_date) + "T13:00"
-    vector_ids = parse_vectors(vector_ids)
-    result = requests.post(
-        url,
-        json={
-            "vectorIds": vector_ids,
-            "startDataPointReleaseDate": start_release_date,
-            "endDataPointReleaseDate": end_release_date
-            }
-        )
-    return result.json()
 
 
 def vectors_to_df(vectors, start_release_date, end_release_date):
