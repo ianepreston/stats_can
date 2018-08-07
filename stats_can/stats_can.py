da5243be32c7a2cecff3363ab6caa6cbce3d48df
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2018-08-07 13:23:17 -0600

    fix to get_tables_for_vectors
    get_series_info_from_vectors now validates the json and just returns the objects, so remove the line that does that in get_tables_from_vectors after calling get_series_info_from_vectors

--- stats_can/stats_can.py
+++ stats_can/stats_can.py
@@ -320,7 +320,6 @@ def get_tables_for_vectors(vectors):
     """
     vectors = parse_vectors(vectors)
     v_json = get_series_info_from_vector(vectors)
-    v_json = [j['object'] for j in v_json]
     tables_list = {j['vectorId']: str(j['productId']) for j in v_json}
     tables_list['all_tables'] = []
     for vector in vectors:
