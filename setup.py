4c2ae62ff4fe9632f034a1254022e19294ed24f4
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2018-08-13 12:49:53 -0600

    add version info
    Should actually pull version info from __init__.py now

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
+from version import find_version
 
 setup(
-  name = 'stats_can',
-  author = 'Ian Preston',
-  description = 'Access StatsCan data with python',
-  license = 'GPL',
+  name='stats_can',
+  author='Ian Preston',
+  description='Access StatsCan data with python',
+  license='GPL',
+  version=find_version('stats_can', '__init__.py'),
   packages = find_packages()
   )
