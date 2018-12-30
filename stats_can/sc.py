fac2e702baa0318d5baf111e4331fb9d1efb4bad
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2018-12-30 15:11:24 -0700

    module order cleanup

--- stats_can/sc.py
+++ stats_can/sc.py
@@ -330,12 +330,6 @@ def h5_update_tables(h5file='stats_can.h5', path=None, tables=None):
     tables: str or list of str, optional, default None
         If included will only update the subset of tables already in the file
         and in the tables parameter
-
-    TODO
-    ----
-    Add a boolean where if given a list of tables and the boolean is true,
-    download any tables in tables that aren't in the h5file already,
-    or should that just be default behaviour?
     """
     if tables:
         local_jsons = metadata_from_h5(tables, h5file=h5file, path=path)
@@ -355,6 +349,42 @@ def h5_update_tables(h5file='stats_can.h5', path=None, tables=None):
             update_table_list.append(local['productId'])
     tables_to_h5(update_table_list, h5file=h5file, path=path)
     return update_table_list
+
+
+def update_tables(path=None, h5file='stats_can.h5', tables=None, csv=True):
+    """Update downloaded tables where required
+
+    Reads local metadata, either from json files or stored in an h5 file,
+    compares it to the metadata on the StatsCan website and downloads those
+    tables that don't have matching metadata
+
+    Just a wrapper for zip_update tables and h5_update_tables functions
+
+    Parameters
+    ----------
+    path: str or path, default None
+        Path where local tables are stored, assumes current directory if None
+    h5file: str, default 'stats_can.h5'
+        Name of the h5 file storing StatsCan tables, set to None for zips
+    tables: list of str, default None
+        For hdf5 only, update only a subset of tables that are both in the file
+        and specified by this argument, None means update all tables
+    csv: boolean, default True
+        If updating zips this determines whether to update zipped CSV or SDMX
+    
+    Returns
+    -------
+    update_table_list: list of str
+        list of updated tables
+    """
+    if h5file:
+        return h5_update_tables(
+            h5file=h5file,
+            path=path,
+            tables=tables
+        )
+    else:
+        return zip_update_tables(path=path, csv=csv)
 
 
 def h5_included_keys(h5file='stats_can.h5', path=None):
@@ -492,37 +522,3 @@ def get_classic_vector_format_df(
     return final_df
 
 
-def update_tables(path=None, h5file='stats_can.h5', tables=None, csv=True):
-    """Update downloaded tables where required
-
-    Reads local metadata, either from json files or stored in an h5 file,
-    compares it to the metadata on the StatsCan website and downloads those
-    tables that don't have matching metadata
-
-    Just a wrapper for zip_update tables and h5_update_tables functions
-
-    Parameters
-    ----------
-    path: str or path, default None
-        Path where local tables are stored, assumes current directory if None
-    h5file: str, default 'stats_can.h5'
-        Name of the h5 file storing StatsCan tables, set to None for zips
-    tables: list of str, default None
-        For hdf5 only, update only a subset of tables that are both in the file
-        and specified by this argument, None means update all tables
-    csv: boolean, default True
-        If updating zips this determines whether to update zipped CSV or SDMX
-    
-    Returns
-    -------
-    update_table_list: list of str
-        list of updated tables
-    """
-    if h5file:
-        return h5_update_tables(
-            h5file=h5file,
-            path=path,
-            tables=tables
-        )
-    else:
-        return zip_update_tables(path=path, csv=csv)
