437b5b674bbce9d9fee9a000e83be984ed4dc021
[Ian Preston] <17241371+ianepreston@users.noreply.github.com>
2019-10-01 12:08:49 -0600

    use new pytest way of doing custom marks

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,19 +10,3 @@ sys.path.insert(0, parent)
 
 here = os.path.dirname(os.path.abspath(__file__))
 os.chdir(here)
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        "--runslow", action="store_true", default=False, help="run slow tests"
-    )
-
-
-def pytest_collection_modifyitems(config, items):
-    if config.getoption("--runslow"):
-        # --runslow given in cli: do not skip slow tests
-        return
-    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
-    for item in items:
-        if "slow" in item.keywords:
-            item.add_marker(skip_slow)
