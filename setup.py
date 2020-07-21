28de27de6d53ca1c8e79be97fd5cd464c3034e4b
Zeheng Li <imzehengl@gmail.com>
2020-07-21 00:02:25 -0600

    setup pypi package build

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
+from os import path
 from setuptools import setup, find_packages
 from version import find_version
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "requirements.txt")) as f:
+    requirements = f.read().splitlines()
 
 setup(
   name='stats_can',
@@ -7,5 +13,6 @@ setup(
   description='Access StatsCan data with python',
   license='GPL',
   version=find_version('stats_can', '__init__.py'),
-  packages=find_packages()
+  packages=find_packages(),
+  install_requires=requirements
 )
