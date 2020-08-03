8c20d29270d8fbd7e60f56f895637d8e3c13a5cf
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2020-08-03 10:21:45 -0600

    switch to poetry

---
+++ noxfile.py
@@ -0,0 +1,47 @@
+import nox
+import tempfile
+
+
+nox.options.sessions = "lint", "safety", "tests"
+
+
+@nox.session(python=["3.8", "3.7"])
+def tests(session):
+    args = session.posargs or ["--cov"]
+    session.run("poetry", "install", external=True)
+    session.run("pytest", *args)
+
+
+locations = "src", "tests", "noxfile.py"
+
+
+@nox.session(python=["3.8", "3.7"])
+def lint(session):
+    args = session.posargs or locations
+    session.install(
+        "flake8", "flake8-black", "flake8-import-order", "pandas-vet", "flake8-bugbear", "flake8-bandit"
+    )
+    session.run("flake8", *args)
+
+
+@nox.session(python="3.8")
+def black(session):
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
+
+
+@nox.session(python="3.8")
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install("safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
