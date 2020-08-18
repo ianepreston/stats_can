3141c7e712a2f791e3e4ec6362c625851cc077ef
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2020-08-18 09:28:49 -0600

    pare down tests more for now

--- noxfile.py
+++ noxfile.py
@@ -18,7 +18,7 @@ def install_with_constraints(session, *args, **kwargs):
         session.install(f"--constraint={requirements.name}", *args, **kwargs)
 
 
-@nox.session(python=["3.8", "3.7"])
+@nox.session(python="3.8")
 def tests(session):
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
