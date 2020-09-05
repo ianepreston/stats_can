c479122251e4a50c5d9a0e70a493d2c21a0659df
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2020-09-05 09:51:04 -0600

    add sessions

--- noxfile.py
+++ noxfile.py
@@ -1,11 +1,24 @@
 import nox
 import tempfile
 
-
+package = "stats_can"
 nox.options.sessions = ["tests"]  # , "lint", "safety"
+locations = "src", "tests", "noxfile.py", "docs/conf.py"
 
 
 def install_with_constraints(session, *args, **kwargs):
+    """Install packages constrained by Poetry's lock file.
+    This function is a wrapper for nox.sessions.Session.install. It
+    invokes pip to install packages inside of the session's virtualenv.
+    Additionally, pip is passed a constraints file generated from
+    Poetry's lock file, to ensure that the packages are pinned to the
+    versions specified in poetry.lock. This allows you to manage the
+    packages as Poetry development dependencies.
+    Arguments:
+        session: The Session object.
+        args: Command-line arguments for pip.
+        kwargs: Additional keyword arguments for Session.install.
+    """
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
             "poetry",
@@ -19,39 +32,34 @@ def install_with_constraints(session, *args, **kwargs):
 
 
 @nox.session(python="3.8")
-def tests(session):
-    args = session.posargs or ["--cov"]
-    session.run("poetry", "install", external=True)
-    session.run("pytest", *args)
-
-
-locations = "src", "tests", "noxfile.py"
-
-
-# @nox.session(python=["3.8", "3.7"])
-# def lint(session):
-#     args = session.posargs or locations
-#     install_with_constraints(
-#         session,
-#         "flake8",
-#         "flake8-black",
-#         "flake8-import-order",
-#         "pandas-vet",
-#         "flake8-bugbear",
-#         "flake8-bandit",
-#     )
-#     session.run("flake8", *args)
-
-
-@nox.session(python="3.8")
 def black(session):
+    """Run black code formatter."""
     args = session.posargs or locations
     install_with_constraints(session, "black")
     session.run("black", *args)
 
 
+@nox.session(python=["3.8", "3.7"])
+def lint(session):
+    """Lint using flake8."""
+    args = session.posargs or locations
+    install_with_constraints(
+        session,
+        "flake8",
+        "flake8-annotations",
+        "flake8-bandit",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-docstrings",
+        "flake8-import-order",
+        "darglint",
+    )
+    session.run("flake8", *args)
+
+
 @nox.session(python="3.8")
 def safety(session):
+    """Scan dependencies for insecure packages."""
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
             "poetry",
@@ -66,9 +74,28 @@ def safety(session):
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
 
 
+@nox.session(python=["3.8", "3.7"])
+def tests(session):
+    """Run the test suite."""
+    args = session.posargs or ["--cov", "-m"]
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(session, "coverage[toml]", "pytest", "pytest-cov")
+    session.run("pytest", *args)
+
+
 @nox.session(python="3.8")
 def coverage(session):
     """Upload coverage data."""
     install_with_constraints(session, "coverage[toml]", "codecov")
     session.run("coverage", "xml", "--fail-under=0")
     session.run("codecov", *session.posargs)
+
+
+@nox.session(python="3.8")
+def docs(session):
+    """Build the documentation."""
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(
+        session, "sphinx", "sphinx-autodoc-typehints", "sphinx_rtd_theme"
+    )
+    session.run("sphinx-build", "docs/source", "docs/_build")
